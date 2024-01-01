dac65dc98fc5d56b66c0754bac8c184686a3b90d
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2024-01-01 22:45:51 +0900

    goのサーバにcorsの設定を追記

--- go-http-server/main.go
+++ go-http-server/main.go
@@ -15,7 +15,22 @@ func main() {
     }
 }
 
+// CORS設定
+func setupCORS(w *http.ResponseWriter, req *http.Request) {
+    (*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "*")
+    (*w).Header().Set("Access-Control-Allow-Headers", "*")
+}
+
 func sampleHandler(w http.ResponseWriter, r *http.Request) {
+    // CORS設定を適用
+    setupCORS(&w, r)
+    // OPTIONSメソッドのリクエストに対しては、処理をここで終了
+    if r.Method == "OPTIONS" {
+        return
+    }
+
+    // 以下は元のハンドラーのロジック
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
     
